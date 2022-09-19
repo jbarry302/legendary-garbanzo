75ed13c904f4b281d829a5c1961cd052f553a8b5
jbarry302 <jbarry302@gmail.com>
2022-09-19 15:19:46 +0800

    done web2

--- web2/fav.php
+++ web2/fav.php
@@ -1,8 +1,29 @@
 <html>
-<head><title>Your Favorite</title></head>
+
+<head>
+    <title>Your Favorite</title>
+</head>
+
 <body>
     <?php
-        
+    if (isset($_POST)) {
+        $username = $_POST['username'] ?? false;
+        $color = $_POST['color'] ?? false;
+        $dish = $_POST['dish'] ?? false;
+
+        if ($username && $color && $dish) {
+            echo "Thanks for your selection " . $username;
+            echo "</br>You really enjoy " . $dish . "</br>- especially with a nice " . $color . " wine";
+        } else {
+            $arr = array(
+                $username = $username ? "" : "username",
+                $color = $color ? "" : "color",
+                $dish = $dish ? "" : "dish",
+            );
+            echo "Invalid input value/s for " . json_encode(array_values(array_filter($arr, 'strlen')));
+        }
+    }
     ?>
 </body>
+
 </html>
