27a7a9ebe0a2a7acc6819a213f7d421d886fba56
jbarry302 <jbarry302@gmail.com>
2022-09-19 16:29:30 +0800

    web5 done;

--- web5/index.php
+++ web5/index.php
@@ -1,5 +1,15 @@
 <?php
+session_start();
+$num_visits = $_SESSION['num_visits'] ?? 1;
 
+if ($num_visits >= 5) {
+    echo "Thank you for visiting the page a lot.</br>";
+}
+if ($num_visits >= 10) {
+    header('Location: congratulations.php');
+}
+
+$_SESSION['num_visits'] = $num_visits + 1;
 ?>
 
 <h1>Hello</h1>
