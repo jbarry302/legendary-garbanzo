62ec0e3a6180a4cb70e07e1ee8571cd75ba89c39
jbarry302 <jbarry302@gmail.com>
2022-09-19 23:06:29 +0800

    done practice 5-06

--- web5/index.php
+++ web5/index.php
@@ -1,15 +1,22 @@
 <?php
 session_start();
-$num_visits = $_SESSION['num_visits'] ?? 1;
 
-if ($num_visits >= 5) {
-    echo "Thank you for visiting the page a lot.</br>";
-}
-if ($num_visits >= 10) {
-    header('Location: congratulations.php');
+if (($_GET['reset'] ?? false) == true) {
+    session_destroy();
+    header('Location: index.php');
+} else {
+    $num_visits = $_SESSION['num_visits'] ?? 1;
+    if ($num_visits >= 5 && $num_visits < 10) {
+        echo "Thank you for visiting the page a lot.</br>";
+    } else if ($num_visits == 10) {
+        header('Location: congratulations.php');
+    } else if ($num_visits > 10) {
+        header('Location: index.php?reset=true');
+    }
+    $_SESSION['num_visits'] = $num_visits + 1;
 }
 
-$_SESSION['num_visits'] = $num_visits + 1;
+
 ?>
 
 <h1>Hello</h1>
